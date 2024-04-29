bf2fbb1cf6f15ff914cf32559e4808745c8ac8a5
Breno Sobral <ssobralbreno@gmail.com>
2024-04-29 06:18:59 -0300

    feat: remove nextjs and add use of reactjs with tailwind

--- README.md
+++ README.md
@@ -1,48 +1,26 @@
 
-
-[DEV] 
-
 ## Introduction
 
-Digi
+Digi Test
 
 ## Technologies Used
 
-- Docker
-- Fastify and NestJS
-- JestJs (Testing framework)
-- TypeORM
-- PostgreSQL (Database)
-- Logging with Winston
+- Tailwindcss
+- ReactJs
+- Vite (Testing framework)
 - Other Libraries: Prettier (Code formatter), ESLint (Linter)
-
-## Requirements
-
-- Docker and Docker Compose
-
-## Documentation
-
-API documentation is available at [API Docs]() once the project is running.
 
 ## Getting Started
 
-To run the Digi project on your local machine, follow these steps:
+To run the DIGI TEST project on your local machine, follow these steps:
 
-1. **Environment Setup**: Create a `.env` file based on the provided `.env-local` template.
-2. **Install Dependencies**: Run `npm install` to install required dependencies.
-3. **Build Containers**: Use `docker-compose build` to build the Docker containers.
-4. **Start Containers**: Execute `docker-compose up -d` to start the containers in detached mode.
-5. **Run Migrations**:  Use `npm run migrate:run` to run migrations.
-6. **Start the Application**: Use `npm run start` to start the application.
-
+1. **Install Dependencies**: Run `yarn` to install required dependencies.
+2. **Start the Application**: Use `yarn dev` to start the application.
 
 ## Testing
 
 To run the test suite, simply execute:
 
 ```bash
-npm test
+yarn test
 ```
-
-## Conclusion
-
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,103 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        principal: "#01080E",
+        white: "#FFFFFF",
+        black: "#000000",
+        header: "#021017",
+        "whatsapp-bg": "#CAAD76",
+        button: "#9FDDFF",
+        inputPlaceholder: "#F4F4F4",
+        textInputPlaceholder: "#C2C1C1",
+
+        green: {
+          50: "#30AF5B",
+          90: "#292C27",
+        },
+        gray: {
+          10: "#EEEEEE",
+          20: "#A2A2A2",
+          30: "#7B7B7B",
+          50: "#585858",
+          90: "#141414",
+        },
+        orange: {
+          50: "#FF814C",
+        },
+        blue: {
+          70: "#021639",
+        },
+        yellow: {
+          50: "#FEC601",
+        },
+      },
+      fontSize: {
+        xs: "0.55rem",
+        sm: "0.65rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "8rem",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "bg-img-1": "url('/img-1.png')",
+        "bg-img-2": "url('/img-2.png')",
+        "feature-bg": "url('/feature-bg.png')",
+        pattern: "url('/pattern.png')",
+        "pattern-2": "url('/pattern-bg.png')",
+      },
+      screens: {
+        xs: "400px",
+        "3xl": "1680px",
+        "4xl": "2200px",
+      },
+      maxWidth: {
+        "10xl": "1512px",
+      },
+      borderRadius: {
+        "5xl": "40px",
       },
     },
   },
   plugins: [],
 };
 export default config;
+
+// theme: {
+//   colors: {
+//     'principal': '#01080E',
+//     white:'#FFFFFF',
+//     black:'#000000',
+//     'header':'#021017',
+//     'whatsapp-bg':'#CAAD76'
+//   },
+//   fontSize: {
+//     xs: '0.55rem',
+//     sm: '0.65rem',
+//     base: '1rem',
+//     lg: '1.125rem',
+//     xl: '1.25rem',
+//     '2xl': '1.5rem',
+//     '3xl': '1.875rem',
+//     '4xl': '2.25rem',
+//     '5xl': '3rem',
+//     '6xl': '8rem',
+//   },
+//   extend: {
+//     backgroundImage: {
+//       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//       "gradient-conic":
+//         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//     },
+//   },
+// },
